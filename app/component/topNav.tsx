f9d2306b67ad9171bf213c56af86c03a85e77d64
mones <mones.cse@gmail.com>
2023-10-06 01:38:17 +0600

    added motion and animation to topnav

--- app/component/topNav.tsx
+++ app/component/topNav.tsx
@@ -1,28 +1,41 @@
 "use client";
 import Link from "next/link";
 import { Links } from "@/lib/data";
-import { useEffect, useState } from "react";
-
+import { useState } from "react";
+import { motion } from "framer-motion";
+interface SelectedNavProps {
+  isActivate: boolean;
+}
+const SelectedNavLink = ({ isActivate }: SelectedNavProps) => {
+  return isActivate ? (
+    <motion.span
+      className="absolute inset-0 bg-gray-100 rounded-full"
+      style={{ zIndex: -1 }}
+      layoutId={"underline"}
+    ></motion.span>
+  ) : null;
+};
 export default function TopNav() {
   const [fragment, setFragment] = useState(window.location.hash);
   return (
-    <div className="bg-white h-24 rounded-lg sm:h-12 w-auto sm:rounded-full mt-8 flex items-center justify-center flex-wrap transition">
+    <div className="bg-white h-24 rounded-lg sm:h-12 w-auto sm:rounded-full mt-8 flex items-center justify-center flex-wrap transition z-10">
       <nav>
-        <ul className="flex gap-6 flex-wrap justify-center px-8 py-1">
+        <motion.ul className="flex gap-6 flex-wrap justify-center px-8 py-1">
           {Links.map((link) => (
-            <li
+            <motion.li
               key={link.hash}
               className={
-                "text-gray-500 hover:cursor-pointer hover:text-gray-950 px-1 transition " +
-                (link.hash == fragment && "bg-gray-200 rounded-lg")
+                "text-gray-500 hover:cursor-pointer hover:text-gray-950 px-1 transition relative"
               }
+              onClick={() => setFragment(link.hash)}
             >
-              <Link href={link.hash} onClick={() => setFragment(link.hash)}>
-                {link.name}
+              <Link href={link.hash}>
+                <span className="text-black px-2">{link.name}</span>
+                <SelectedNavLink isActivate={fragment === link.hash} />
               </Link>
-            </li>
+            </motion.li>
           ))}
-        </ul>
+        </motion.ul>
       </nav>
     </div>
   );
