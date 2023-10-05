1e3b40b85acb1797687e2255c45c711e98449e2b
mones <mones.cse@gmail.com>
2023-10-06 00:36:29 +0600

    adding activate link css

--- app/component/topNav.tsx
+++ app/component/topNav.tsx
@@ -1,8 +1,10 @@
-// "use client";
+"use client";
 import Link from "next/link";
 import { Links } from "@/lib/data";
+import { useEffect, useState } from "react";
 
 export default function TopNav() {
+  const [fragment, setFragment] = useState(window.location.hash);
   return (
     <div className="bg-white h-24 rounded-lg sm:h-12 w-auto sm:rounded-full mt-8 flex items-center justify-center flex-wrap transition">
       <nav>
@@ -10,10 +12,14 @@ export default function TopNav() {
           {Links.map((link) => (
             <li
               key={link.hash}
-              className="text-gray-500 hover:cursor-pointer hover:text-gray-950
-            "
+              className={
+                "text-gray-500 hover:cursor-pointer hover:text-gray-950 px-1 transition " +
+                (link.hash == fragment && "bg-gray-200 rounded-lg")
+              }
             >
-              <Link href={link.hash}>{link.name}</Link>
+              <Link href={link.hash} onClick={() => setFragment(link.hash)}>
+                {link.name}
+              </Link>
             </li>
           ))}
         </ul>
