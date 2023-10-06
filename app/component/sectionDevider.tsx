707bc72a1b43b8290a523d2284a8c71a372320b2
mones <mones.cse@gmail.com>
2023-10-06 16:23:28 +0600

    about me

---
+++ app/component/sectionDevider.tsx
@@ -0,0 +1,7 @@
+const SectionDevider = () => {
+  return (
+    <div className="bg-gray-300 w-1 h-12 my-20 rounded-full hidden sm:block"></div>
+  );
+};
+
+export default SectionDevider;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import { Links } from "@/lib/data";
 import TopNav from "@/app/component/topNav";
 import Intro from "@/app/component/intro";
+import SectionDevider from "./component/SectionDevider";
+import AboutMe from "./component/aboutMe";
 
 export default function Portfolio() {
   return (
     <main className="w-full h-screen flex items-center flex-col">
       <TopNav />
       <Intro />
+      <SectionDevider />
+      <AboutMe />
+      <div className="h-400">1</div>
     </main>
   );
 }
